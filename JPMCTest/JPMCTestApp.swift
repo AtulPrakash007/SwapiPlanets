5e6712178b70671020371b1095a6b764754b884c
Atul Prakash <atul.prakash@concentrix.com>
2023-05-24 12:03:28 +0530

    Initial Commit

---
+++ JPMCTest/JPMCTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPMCTestApp.swift
+//  JPMCTest
+//
+//  Created by Atul Prakash on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct JPMCTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
